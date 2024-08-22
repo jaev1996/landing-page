2b99f8d49c2c4118e85e7d4cd124b2cc1e6a9abe
Jaev <escalonavielmajose@gmail.com>
2024-08-22 11:35:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {Button} from "@nextui-org/button";
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        TU PUTA MADRE WEY!!
+      </h1>
+      <Button color="primary">
+        Button
+      </Button>
+    </>
+  )
+}
+
+export default App
